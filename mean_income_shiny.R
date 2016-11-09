bf8bc9073985499438cfce1759f45d887b6792c5
awunderground <williamsar6@vcu.edu>
2016-11-09 15:26:54 -0500

    Add factor to shiny so axis labels are properly ordered

--- mean_income_shiny.R
+++ mean_income_shiny.R
@@ -5,16 +5,25 @@ library(extrafont)
 library(grid)
 library(RColorBrewer)
 
-Sys.setenv(R_GSCMD = "C:\\Program Files\\gs\\gs9.20\\bin\\gswin64.exe")
+#Sys.setenv(R_GSCMD = "C:\\Program Files\\gs\\gs9.20\\bin\\gswin64.exe")
 
-# Urban theme for Windows
-source('https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/temp-windows/urban_ggplot_theme.R')
+# Source file for Windows
+#source('https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/temp-windows/urban_ggplot_theme.R')
+#source('urban_theme_windows.R')
 
+
+# Source file for Mac
+#source('https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/master/urban_ggplot_theme.R')
+source('urban_theme_mac.R')
 
 # Load data
-mean.income <- tbl_df(read.csv("data\\mean.income.csv", header = TRUE, stringsAsFactors = TRUE))
-dollar.change <- tbl_df(read.csv("data\\dollar.change.csv", header = TRUE, stringsAsFactors = TRUE))
-percent.change <- tbl_df(read.csv("data\\percent.change.csv", header = TRUE, stringsAsFactors = TRUE))
+#mean.income <- tbl_df(read.csv("data\\mean.income.csv", header = TRUE, stringsAsFactors = FALSE))
+#dollar.change <- tbl_df(read.csv("data\\dollar.change.csv", header = TRUE, stringsAsFactors = FALSE))
+#percent.change <- tbl_df(read.csv("data\\percent.change.csv", header = TRUE, stringsAsFactors = FALSE))
+
+mean.income <- tbl_df(read.csv("data/mean.income.csv", header = TRUE, stringsAsFactors = FALSE))
+dollar.change <- tbl_df(read.csv("data/dollar.change.csv", header = TRUE, stringsAsFactors = FALSE))
+percent.change <- tbl_df(read.csv("data/percent.change.csv", header = TRUE, stringsAsFactors = FALSE))
 
 # 
 
@@ -27,8 +36,13 @@ dollar.change <- dollar.change %>%
 percent.change <- percent.change %>%
   mutate(comparison = "percent.change")
 
-mean.income <- bind_rows(mean.income, dollar.change, percent.change)
-
+mean.income <- bind_rows(mean.income, dollar.change, percent.change) %>%
+  mutate(group = gsub("1\\.", "", group)) %>%
+  mutate(group = gsub("2\\.", "", group)) %>%
+  mutate(group = gsub("3\\.", "", group)) %>%
+  mutate(group = gsub("4\\.", "", group)) %>%
+  mutate(group = factor(group, levels = group))
+  
 ##
 ## SHINY
 ##
@@ -79,14 +93,26 @@ server <- function(input, output){
   
   output$chart <- renderPlot({
 
+    
+    title <- if (input$measure == "per capita annuity") {
+                 "Per Capita Annuity"} 
+             else if (input$measure == "per capita cash") {
+               "Per Capita Cash"}
+             else if (input$measure == "net per capita annuity") {
+               "Net Per Capita Annuity"
+               }
+             else if (input$measure == "net per capita cash") {
+             "Net Per Capita Cash"}
+    
     mean.income %>%
       filter(comparison == input$comparison) %>%
       filter(measure == input$measure) %>%
       filter(category == input$demographic) %>%
       ggplot(aes(year, value, color = group)) +
-      scale_y_continuous(expand = c(0,0)) +
       geom_line() +
-      ggtitle("Per Capita Annuity")
+      ggtitle(title) +
+      xlab("Year") +
+      geom_line(size = 1)
   
   })
   
--- mean_income_static.R
+++ mean_income_static.R
@@ -11,19 +11,46 @@ Sys.setenv(R_GSCMD = "C:\\Program Files\\gs\\gs9.20\\bin\\gswin64.exe")
 
 # Source file for Windows
 #source('https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/temp-windows/urban_ggplot_theme.R')
-source('urban_theme_windows.R')
+#source('urban_theme_windows.R')
 
 # Source file for Mac
 #source('https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/master/urban_ggplot_theme.R')
+source('urban_theme_mac.R')
 
 options(scipen=999)
 
 
 
 # Load data
-mean.income <- tbl_df(read.csv("data\\mean.income.csv", header = TRUE, stringsAsFactors = TRUE))
-dollar.change <- tbl_df(read.csv("data\\dollar.change.csv", header = TRUE, stringsAsFactors = TRUE))
-percent.change <- tbl_df(read.csv("data\\percent.change.csv", header = TRUE, stringsAsFactors = TRUE))
+#mean.income <- tbl_df(read.csv("data\\mean.income.csv", header = TRUE, stringsAsFactors = TRUE))
+#dollar.change <- tbl_df(read.csv("data\\dollar.change.csv", header = TRUE, stringsAsFactors = TRUE))
+#percent.change <- tbl_df(read.csv("data\\percent.change.csv", header = TRUE, stringsAsFactors = TRUE))
+
+mean.income <- tbl_df(read.csv("data/mean.income.csv", header = TRUE, stringsAsFactors = TRUE))
+dollar.change <- tbl_df(read.csv("data/dollar.change.csv", header = TRUE, stringsAsFactors = TRUE))
+percent.change <- tbl_df(read.csv("data/percent.change.csv", header = TRUE, stringsAsFactors = TRUE))
+
+
+
+
+
+mean.income <- mean.income %>%
+  mutate(comparison = "mean.income")
+
+dollar.change <- dollar.change %>%
+  mutate(comparison = "dollar.change")
+
+percent.change <- percent.change %>%
+  mutate(comparison = "percent.change")
+
+mean.income <- bind_rows(mean.income, dollar.change, percent.change) %>%
+  mutate(group = gsub("1\\.", "", group)) %>%
+  mutate(group = gsub("2\\.", "", group)) %>%
+  mutate(group = gsub("3\\.", "", group)) %>%
+  mutate(group = gsub("4\\.", "", group))
+
+
+
 
 mean.income %>%
   filter(measure == "per capita annuity") %>%

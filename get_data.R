8743874821e7002ccc5fa91892f4d02155a93d17
awunderground <awunderground@gmail.com>
2016-11-09 11:46:52 -0500

    Add dollar.change and percent.change

--- get_data.R
+++ get_data.R
@@ -12,6 +12,12 @@ library(readxl)
 
 # Read unformatted data from Microsoft Excel
 mean.income <- read_excel("X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx", sheet = "mean income", skip = 2, col_names = TRUE)
+dollar.change <- read_excel("X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx", sheet = "$mean income compare", skip = 2, col_names = TRUE)
+percent.change <- read_excel("X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx", sheet = "%mean income compare", skip = 2, col_names = TRUE)
+
+##
+## MEAN INCOME
+##
 
 # Per capita annuity
 per.capita.annuity <- mean.income[, 1:9]
@@ -95,8 +101,192 @@ net.per.capita.cash <- net.per.capita.cash %>%
 
 mean.income <- bind_rows(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
 
+rm(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
+
 mean.income <- gather(mean.income, key = year , value,-category, -group, -category, -measure) %>%
                mutate(year = as.numeric(gsub("year", "", year)))
+
+##
+## DOLLAR CHANGE#
+##
+
+# Per capita annuity
+per.capita.annuity <- dollar.change[, 1:9]
+
+names(per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+per.capita.annuity <- per.capita.annuity %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "per capita annuity")
+
+# Per capita cash
+per.capita.cash <- dollar.change[, 10:18]
+
+names(per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+per.capita.cash <- per.capita.cash %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "per capita cash")
+
+# Net per capita annuity
+net.per.capita.annuity <- dollar.change[, 19:27]
+
+names(net.per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+net.per.capita.annuity <- net.per.capita.annuity %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "net per capita annuity")
+
+# Net per capita cash
+net.per.capita.cash <- dollar.change[, 28:36]
+
+names(net.per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+net.per.capita.cash <- net.per.capita.cash %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "net per capita cash")
+
+dollar.change <- bind_rows(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
+
+rm(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
+
+dollar.change <- gather(dollar.change, key = year , value,-category, -group, -category, -measure) %>%
+  mutate(year = as.numeric(gsub("year", "", year)))
+
+##
+## PERCENT CHANGE
+##
+
+# Per capita annuity
+per.capita.annuity <- percent.change[, 1:9]
+
+names(per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+per.capita.annuity <- per.capita.annuity %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "per capita annuity")
+
+# Per capita cash
+per.capita.cash <- percent.change[, 10:18]
+
+names(per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+per.capita.cash <- per.capita.cash %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "per capita cash")
+
+# Net per capita annuity
+net.per.capita.annuity <- percent.change[, 19:27]
+
+names(net.per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+net.per.capita.annuity <- net.per.capita.annuity %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "net per capita annuity")
+
+# Net per capita cash
+net.per.capita.cash <- percent.change[, 28:36]
+
+names(net.per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+net.per.capita.cash <- net.per.capita.cash %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "net per capita cash")
+
+percent.change <- bind_rows(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
+
+rm(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
+
+percent.change <- gather(percent.change, key = year , value,-category, -group, -category, -measure) %>%
+  mutate(year = as.numeric(gsub("year", "", year)))
 
 # If data directory does not exist, create data directory
 if (!file.exists("data")) {
@@ -105,3 +295,5 @@ if (!file.exists("data")) {
 
 # Write .csv
 write.csv(mean.income, "data//mean.income.csv", row.names = FALSE)
+write.csv(dollar.change, "data//dollar.change.csv", row.names = FALSE)
+write.csv(percent.change, "data//percent.change.csv", row.names = FALSE)

3d06ca5bbcdda98220275157fe7ae2dc87e11242
awunderground <awunderground@gmail.com>
2016-11-09 08:54:37 -0500

    Read mean income from Excel file and sort into four clean(ish) data frames

--- get_data.R
+++ get_data.R
@@ -1,7 +1,93 @@
 library(tidyverse)
 library(readxl)
 
-read_excel("X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx"
+mean.income <- read_excel("X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx", sheet = "mean income", skip = 2, col_names = TRUE)
+
+# Per capita annuity
+per.capita.annuity <- mean.income[, 1:9]
+
+names(per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+per.capita.annuity <- per.capita.annuity %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "per capita annuity")
+
+
+# Per capita cash
+per.capita.cash <- mean.income[, 10:18]
+
+names(per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+per.capita.cash <- per.capita.cash %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "per capita cash")
+
+# Net per capita annuity
+net.per.capita.annuity <- mean.income[, 19:27]
+
+names(net.per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+net.per.capita.annuity <- net.per.capita.annuity %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "net per capita annuity")
+
+# Net per capita cash
+net.per.capita.cash <- mean.income[, 28:36]
+
+names(net.per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+
+net.per.capita.cash <- net.per.capita.cash %>%
+  select(-trash) %>%
+  filter(!is.na(group)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
+  filter(!is.na(year2015)) %>%
+  mutate(measure = "net per capita cash")
+
+
+
+
+
 
 
 

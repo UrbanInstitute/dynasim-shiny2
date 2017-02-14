6a81fc4d0bcba6617ee420ec03f66a2906e42954
awunderground <awunderground@gmail.com>
2017-02-14 16:11:47 -0500

    Add percent and dollar change calculations to program and remove code that adds those measures from the Excel files.

--- get_data.R
+++ get_data.R
@@ -10,49 +10,44 @@
 library(tidyverse)
 library(readxl)
 
-# Read unformatted data from Microsoft Excel
-scheduled.dollar.change <- read_excel("X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx", sheet = "$mean income compare", skip = 2, col_names = TRUE)
-scheduled.percent.change <- read_excel("X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx", sheet = "%mean income compare", skip = 2, col_names = TRUE)
+options(scipen = 999)
 
-
-
-
-
-
-files <- read_csv("option, scale, link
-          bpc, per capita, X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx
-          bpc, equivalent, X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsEquivalentRun5SaveOpt4.xlsx
-          0, per capita, X:\\programs\\Run912\\opt0\\BPCtableShellsOPT0.xlsx
-          0, equivalent, X:\\programs\\Run912\\opt0\\BPCtableShellsEquivalentOPT0.xlsx
-          1, per capita, X:\\programs\\Run912\\opt1(MiniPIA)\\BPCtableShellsOPT1.xlsx
-          1, equivalent, X:\\programs\\Run912\\opt1(MiniPIA)\\BPCtableShellsEquivalentOPT1.xlsx
-          2, per capita, X:\\programs\\Run912\\opt2(increaseTaxSSB)\\BPCtableShellsOPT2.xlsx
-          2, equivalent, X:\\programs\\Run912\\opt2(increaseTaxSSB)\\BPCtableShellsEquivalentOPT2.xlsx
-          3, per capita, X:\\programs\\Run912\\opt3(capSpouse)\\BPCtableShellsOPT3.xlsx
-          3, equivalent, X:\\programs\\Run912\\opt3(capSpouse)\\BPCtableShellsEquivalentOPT3.xlsx
-          4, per capita, X:\\programs\\Run912\\opt4(survivorjs75)\\BPCtableShellsOPT4.xlsx
-          4, equivalent, X:\\programs\\Run912\\opt4(survivorjs75)\\BPCtableShellsEquivalentOPT4.xlsx
-          5, per capita, X:\\programs\\Run912\\opt5(increaseTAXMAX)\\BPCtableShellsOPT5.xlsx
-          5, equivalent, X:\\programs\\Run912\\opt5(increaseTAXMAX)\\BPCtableShellsEquivalentOPT5.xlsx
-          5b, per capita, X:\\programs\\Run912\\opt5B(RaiseFICA)\\BPCtableShellsOPT5B.xlsx
-          5b, equivalent, X:\\programs\\Run912\\opt5B(RaiseFICA)\\BPCtableShellsEquivalentOPT5B.xlsx
-          5c, per capita, X:\\programs\\Run912\\opt5c(RaiseFICAonly)\\BPCtableShellsOPT5c.xlsx
-          5c, equivalent, X:\\programs\\Run912\\opt5c(RaiseFICAonly)\\BPCtableShellsEquivalentOPT5c.xlsx
-          6, per capita, X:\\programs\\Run912\\opt6(decreaseCOLA)\\BPCtableShellsOPT6.xlsx
-          6, equivalent, X:\\programs\\Run912\\opt6(decreaseCOLA)\\BPCtableShellsEquivalentOPT6.xlsx
-          7, per capita, X:\\programs\\Run912\\opt7(IncreaseFRA)\\BPCtableShellsOPT7.xlsx
-          7, equivalent, X:\\programs\\Run912\\opt7(IncreaseFRA)\\BPCtableShellsEquivalentOPT7.xlsx
-          8, per capita, X:\\programs\\Run912\\opt8(IncreaseFRAERA)\\BPCtableShellsOPT8.xlsx
-          8, equivalent, X:\\programs\\Run912\\opt8(IncreaseFRAERA)\\BPCtableShellsEquivalentOPT8.xlsx
-          9, per capita, X:\\programs\\Run912\\opt9\\BPCtableShellsOPT9v12.xlsx
-          9, equivalent, X:\\programs\\Run912\\opt9\\BPCtableShellsEquivalentOPT9v12.xlsx
-          10, per capita, X:\\programs\\Run912\\opt10\\BPCtableShellsOPT10v5.xlsx
-          10, equivalent, X:\\programs\\Run912\\opt10\\BPCtableShellsEQUIVALENTOPT10v5.xlsx
-          12, per capita, X:\\programs\\Run912\\opt12\\BPCtableShellsOPT12.xlsx
-          12, equivalent, X:\\programs\\Run912\\opt12\\BPCtableShellsEquivalentOPT12.xlsx")
+files <- read_csv("option, scale, base, link
+          bpc, per capita, level, X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx
+          bpc, equivalent, level, X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsEquivalentRun5SaveOpt4.xlsx
+          0, per capita, payable, X:\\programs\\Run912\\opt0\\BPCtableShellsOPT0.xlsx
+          0, equivalent, payable, X:\\programs\\Run912\\opt0\\BPCtableShellsEquivalentOPT0.xlsx
+          0, per capita, scheduled, X:\\programs\\Run912\\payable\\BPCTableShellsPayableV3.xlsx
+          0, equivalent, scheduled, X:\\programs\\Run912\\payable\\BPCTableShellsEquivalentPayableV3.xlsx
+          1, per capita, level, X:\\programs\\Run912\\opt1(MiniPIA)\\BPCtableShellsOPT1.xlsx
+          1, equivalent, level, X:\\programs\\Run912\\opt1(MiniPIA)\\BPCtableShellsEquivalentOPT1.xlsx
+          2, per capita, level, X:\\programs\\Run912\\opt2(increaseTaxSSB)\\BPCtableShellsOPT2.xlsx
+          2, equivalent, level, X:\\programs\\Run912\\opt2(increaseTaxSSB)\\BPCtableShellsEquivalentOPT2.xlsx
+          3, per capita, level, X:\\programs\\Run912\\opt3(capSpouse)\\BPCtableShellsOPT3.xlsx
+          3, equivalent, level, X:\\programs\\Run912\\opt3(capSpouse)\\BPCtableShellsEquivalentOPT3.xlsx
+          4, per capita, level, X:\\programs\\Run912\\opt4(survivorjs75)\\BPCtableShellsOPT4.xlsx
+          4, equivalent, level, X:\\programs\\Run912\\opt4(survivorjs75)\\BPCtableShellsEquivalentOPT4.xlsx
+          5, per capita, level, X:\\programs\\Run912\\opt5(increaseTAXMAX)\\BPCtableShellsOPT5.xlsx
+          5, equivalent, level, X:\\programs\\Run912\\opt5(increaseTAXMAX)\\BPCtableShellsEquivalentOPT5.xlsx
+          5b, per capita, level, X:\\programs\\Run912\\opt5B(RaiseFICA)\\BPCtableShellsOPT5B.xlsx
+          5b, equivalent, level, X:\\programs\\Run912\\opt5B(RaiseFICA)\\BPCtableShellsEquivalentOPT5B.xlsx
+          5c, per capita, level, X:\\programs\\Run912\\opt5c(RaiseFICAonly)\\BPCtableShellsOPT5c.xlsx
+          5c, equivalent, level, X:\\programs\\Run912\\opt5c(RaiseFICAonly)\\BPCtableShellsEquivalentOPT5c.xlsx
+          6, per capita, level, X:\\programs\\Run912\\opt6(decreaseCOLA)\\BPCtableShellsOPT6.xlsx
+          6, equivalent, level, X:\\programs\\Run912\\opt6(decreaseCOLA)\\BPCtableShellsEquivalentOPT6.xlsx
+          7, per capita, level, X:\\programs\\Run912\\opt7(IncreaseFRA)\\BPCtableShellsOPT7.xlsx
+          7, equivalent, level, X:\\programs\\Run912\\opt7(IncreaseFRA)\\BPCtableShellsEquivalentOPT7.xlsx
+          8, per capita, level, X:\\programs\\Run912\\opt8(IncreaseFRAERA)\\BPCtableShellsOPT8.xlsx
+          8, equivalent, level, X:\\programs\\Run912\\opt8(IncreaseFRAERA)\\BPCtableShellsEquivalentOPT8.xlsx
+          9, per capita, level, X:\\programs\\Run912\\opt9\\BPCtableShellsOPT9v12.xlsx
+          9, equivalent, level, X:\\programs\\Run912\\opt9\\BPCtableShellsEquivalentOPT9v12.xlsx
+          10, per capita, level, X:\\programs\\Run912\\opt10\\BPCtableShellsOPT10v5.xlsx
+          10, equivalent, level, X:\\programs\\Run912\\opt10\\BPCtableShellsEQUIVALENTOPT10v5.xlsx
+          12, per capita, level, X:\\programs\\Run912\\opt12\\BPCtableShellsOPT12.xlsx
+          12, equivalent, level, X:\\programs\\Run912\\opt12\\BPCtableShellsEquivalentOPT12.xlsx")
         
 works <- read_excel("X:\\programs\\Run912\\opt1(MiniPIA)\\BPCtableShellsEquivalentOPT1.xlsx", sheet = "mean income", skip = 2, col_names = TRUE)
-broken <- read_excel("X:\\programs\\Run912\\opt10\\BPCtableShellsEQUIVALENTOPT10v5.xlsx", sheet = "mean income", skip = 2, col_names = TRUE)
+broken <- read_excel("X:\\programs\\Run912\\payable\\BPCTableShellsEquivalentPayableV3.xlsx", sheet = "mean income", skip = 2, col_names = TRUE)
 
 ##
 ## MEAN INCOME
@@ -147,8 +142,7 @@ meanIncomeScrapeR <- function(link) {
   rm(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
   
   mean.income <- gather(mean.income, key = year , value,-category, -group, -category, -measure) %>%
-                 mutate(year = as.numeric(gsub("year", "", year))) %>%
-                 mutate(chart = "mean.income")
+                 mutate(year = as.numeric(gsub("year", "", year)))
   
   return(mean.income)
 }
@@ -159,17 +153,18 @@ final.income <- tibble()
 
 for (i in 1:nrow(files)) {
  
-  mean.income <- meanIncomeScrapeR(as.character(files[i, 3]))
+  mean.income <- meanIncomeScrapeR(as.character(files[i, 4]))
   
   mean.income <- mean.income %>%
     mutate(option = as.character(files[i, 1])) %>%
-    mutate(scale = as.character(files[i, 2]))
+    mutate(scale = as.character(files[i, 2])) %>%
+    mutate(base = as.character(files[i, 3]))
   
   final.income <- bind_rows(final.income, mean.income)
    
 }
 
-# 46,080 rows
+# 1,536 * 32 = 49,152 rows
 #table(final.income$category)
 #table(final.income$measure)
 #table(final.income$year)
@@ -183,194 +178,40 @@ for (i in 1:nrow(files)) {
 
 
 
-# Fix imperfections from read_excel
-scheduled.dollar.change <- scheduled.dollar.change[, c(1:27, 29:37)]
-scheduled.percent.change <- scheduled.percent.change[, c(1:27, 29:37)]
 
 
-##
-## DOLLAR CHANGE
-##
+## PER CAPITA
 
-# Per capita annuity
-per.capita.annuity <- scheduled.dollar.change[, 1:9]
+per.capita <- final.income %>%
+  filter(scale == "per capita" & option != "0") %>%
+  select(-base)
 
-names(per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+per.capita.0 <- final.income %>%
+  filter(scale == "per capita" & option == "0") %>%
+  spread(key = option, value = value) %>%
+  rename(Option0 = `0`)
 
-per.capita.annuity <- per.capita.annuity %>%
-  select(-trash) %>%
-  filter(!is.na(group)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  filter(!is.na(year2015)) %>%
-  mutate(measure = "per capita annuity")
+final.income <- left_join(per.capita, per.capita.0, by = c("category", "group", "measure", "year", "scale"))
 
-# Per capita cash
-per.capita.cash <- scheduled.dollar.change[, 10:18]
+final.income <- final.income %>%
+  mutate(dollar.change = value - Option0) %>%
+  mutate(percent.change = (value - Option0) / Option0) %>%
+  select(-Option0)
 
-names(per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
+per.capita.0 <- per.capita.0 %>%
+  rename(value = Option0) %>%
+  mutate(option = "0") %>%
+  mutate(dollar.change = 0) %>%
+  mutate(percent.change = 0)
 
-per.capita.cash <- per.capita.cash %>%
-  select(-trash) %>%
-  filter(!is.na(group)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  filter(!is.na(year2015)) %>%
-  mutate(measure = "per capita cash")
+final.income <- union(final.income, per.capita.0)
 
-# Net per capita annuity
-net.per.capita.annuity <- scheduled.dollar.change[, 19:27]
 
-names(net.per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
 
-net.per.capita.annuity <- net.per.capita.annuity %>%
-  select(-trash) %>%
-  filter(!is.na(group)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  filter(!is.na(year2015)) %>%
-  mutate(measure = "net per capita annuity")
 
-# Net per capita cash
-net.per.capita.cash <- scheduled.dollar.change[, 28:36]
 
-names(net.per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
 
-net.per.capita.cash <- net.per.capita.cash %>%
-  select(-trash) %>%
-  filter(!is.na(group)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  filter(!is.na(year2015)) %>%
-  mutate(measure = "net per capita cash")
 
-scheduled.dollar.change <- bind_rows(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
-
-rm(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
-
-scheduled.dollar.change <- gather(scheduled.dollar.change, key = year , value,-category, -group, -category, -measure) %>%
-  mutate(year = as.numeric(gsub("year", "", year))) %>%
-  mutate(chart = "scheduled.dollar.change")
-
-##
-## PERCENT CHANGE
-##
-
-# Per capita annuity
-per.capita.annuity <- scheduled.percent.change[, 1:9]
-
-names(per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
-
-per.capita.annuity <- per.capita.annuity %>%
-  select(-trash) %>%
-  filter(!is.na(group)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  filter(!is.na(year2015)) %>%
-  mutate(measure = "per capita annuity")
-
-# Per capita cash
-per.capita.cash <- scheduled.percent.change[, 10:18]
-
-names(per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
-
-per.capita.cash <- per.capita.cash %>%
-  select(-trash) %>%
-  filter(!is.na(group)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  filter(!is.na(year2015)) %>%
-  mutate(measure = "per capita cash")
-
-# Net per capita annuity
-net.per.capita.annuity <- scheduled.percent.change[, 19:27]
-
-names(net.per.capita.annuity) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
-
-net.per.capita.annuity <- net.per.capita.annuity %>%
-  select(-trash) %>%
-  filter(!is.na(group)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  filter(!is.na(year2015)) %>%
-  mutate(measure = "net per capita annuity")
-
-# Net per capita cash
-net.per.capita.cash <- scheduled.percent.change[, 28:36]
-
-names(net.per.capita.cash) <- c("category", "trash", "group", "year2015", "year2025", "year2035", "year2045", "year2055", "year2065")
-
-net.per.capita.cash <- net.per.capita.cash %>%
-  select(-trash) %>%
-  filter(!is.na(group)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  mutate(category = ifelse(is.na(category), lag(category), category)) %>%
-  filter(!is.na(year2015)) %>%
-  mutate(measure = "net per capita cash")
-
-scheduled.percent.change <- bind_rows(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
-
-rm(per.capita.annuity, per.capita.cash, net.per.capita.annuity, net.per.capita.cash)
-
-scheduled.percent.change <- gather(scheduled.percent.change, key = year , value,-category, -group, -category, -measure) %>%
-  mutate(year = as.numeric(gsub("year", "", year))) %>%
-  mutate(chart = "scheduled.percent.change")
 
 # If data directory does not exist, create data directory
 if (!file.exists("data")) {
@@ -379,9 +220,9 @@ if (!file.exists("data")) {
 
 # Combine data sets
 
-income <- bind_rows(mean.income, scheduled.dollar.change, scheduled.percent.change)
 
-write_csv(income, "data//income.csv")
+
+#write_csv(income, "data//income.csv")
 
 # Write .csv
 #write_csv(mean.income, "data//mean.income.csv", row.names = FALSE)

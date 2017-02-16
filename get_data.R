bae94eceb39194fed4e90c6eea0e2cd0906cb99f
awunderground <awunderground@gmail.com>
2017-02-16 10:12:17 -0500

    get_data.R now pulls options, scales, and links from a user-defined list created in options_guide.xlsx.

--- get_data.R
+++ get_data.R
@@ -1,7 +1,7 @@
 # Aaron Williams, Urban Institute Program on Retirement Policy
 
 # This script reads mean income data for 64 groups, across 4 measures, in 6 
-# different decades, for 15 different reform options, in two scales. It then 
+# different decades, for user-defined reform options, in two scales. It then 
 # calculates percent and dollar change against two different baselines. 
 # The script also cleans the data and turns them into a long data frame 
 # formatted for ggplot2 
@@ -12,41 +12,10 @@ library(readxl)
 
 options(scipen = 999)
 
-# Create a df with links to the Excel sheets with the mean income data
-files <- read_csv("option, scale, base, link
-          bpc, per capita, level, X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsRun5SaveOpt4withSUPERTAX.xlsx
-          bpc, equivalent, level, X:\\programs\\Run912\\run5SaveOpt4\\BPCtableShellsEquivalentRun5SaveOpt4.xlsx
-          0, per capita, scheduled, X:\\programs\\Run912\\opt0\\BPCtableShellsOPT0.xlsx
-          0, equivalent, scheduled, X:\\programs\\Run912\\opt0\\BPCtableShellsEquivalentOPT0.xlsx
-          0, per capita, payable, X:\\programs\\Run912\\payable\\BPCTableShellsPayableV3.xlsx
-          0, equivalent, payable, X:\\programs\\Run912\\payable\\BPCTableShellsEquivalentPayableV3.xlsx
-          1, per capita, level, X:\\programs\\Run912\\opt1(MiniPIA)\\BPCtableShellsOPT1.xlsx
-          1, equivalent, level, X:\\programs\\Run912\\opt1(MiniPIA)\\BPCtableShellsEquivalentOPT1.xlsx
-          2, per capita, level, X:\\programs\\Run912\\opt2(increaseTaxSSB)\\BPCtableShellsOPT2.xlsx
-          2, equivalent, level, X:\\programs\\Run912\\opt2(increaseTaxSSB)\\BPCtableShellsEquivalentOPT2.xlsx
-          3, per capita, level, X:\\programs\\Run912\\opt3(capSpouse)\\BPCtableShellsOPT3.xlsx
-          3, equivalent, level, X:\\programs\\Run912\\opt3(capSpouse)\\BPCtableShellsEquivalentOPT3.xlsx
-          4, per capita, level, X:\\programs\\Run912\\opt4(survivorjs75)\\BPCtableShellsOPT4.xlsx
-          4, equivalent, level, X:\\programs\\Run912\\opt4(survivorjs75)\\BPCtableShellsEquivalentOPT4.xlsx
-          5, per capita, level, X:\\programs\\Run912\\opt5(increaseTAXMAX)\\BPCtableShellsOPT5.xlsx
-          5, equivalent, level, X:\\programs\\Run912\\opt5(increaseTAXMAX)\\BPCtableShellsEquivalentOPT5.xlsx
-          5b, per capita, level, X:\\programs\\Run912\\opt5B(RaiseFICA)\\BPCtableShellsOPT5B.xlsx
-          5b, equivalent, level, X:\\programs\\Run912\\opt5B(RaiseFICA)\\BPCtableShellsEquivalentOPT5B.xlsx
-          5c, per capita, level, X:\\programs\\Run912\\opt5c(RaiseFICAonly)\\BPCtableShellsOPT5c.xlsx
-          5c, equivalent, level, X:\\programs\\Run912\\opt5c(RaiseFICAonly)\\BPCtableShellsEquivalentOPT5c.xlsx
-          6, per capita, level, X:\\programs\\Run912\\opt6(decreaseCOLA)\\BPCtableShellsOPT6.xlsx
-          6, equivalent, level, X:\\programs\\Run912\\opt6(decreaseCOLA)\\BPCtableShellsEquivalentOPT6.xlsx
-          7, per capita, level, X:\\programs\\Run912\\opt7(IncreaseFRA)\\BPCtableShellsOPT7.xlsx
-          7, equivalent, level, X:\\programs\\Run912\\opt7(IncreaseFRA)\\BPCtableShellsEquivalentOPT7.xlsx
-          8, per capita, level, X:\\programs\\Run912\\opt8(IncreaseFRAERA)\\BPCtableShellsOPT8.xlsx
-          8, equivalent, level, X:\\programs\\Run912\\opt8(IncreaseFRAERA)\\BPCtableShellsEquivalentOPT8.xlsx
-          9, per capita, level, X:\\programs\\Run912\\opt9\\BPCtableShellsOPT9v12.xlsx
-          9, equivalent, level, X:\\programs\\Run912\\opt9\\BPCtableShellsEquivalentOPT9v12.xlsx
-          10, per capita, level, X:\\programs\\Run912\\opt10\\BPCtableShellsOPT10v5.xlsx
-          10, equivalent, level, X:\\programs\\Run912\\opt10\\BPCtableShellsEQUIVALENTOPT10v5.xlsx
-          12, per capita, level, X:\\programs\\Run912\\opt12\\BPCtableShellsOPT12.xlsx
-          12, equivalent, level, X:\\programs\\Run912\\opt12\\BPCtableShellsEquivalentOPT12.xlsx")
-        
+# Read df with links to the Excel sheets with the mean income data
+files <- read_excel("options_guide.xlsx") %>%
+  select(option, scale, link)
+
 ##
 ## MEAN INCOME
 ##
@@ -151,17 +120,16 @@ final.income <- tibble()
 
 for (i in 1:nrow(files)) {
  
-  mean.income <- meanIncomeScrapeR(as.character(files[i, 4]))
+  mean.income <- meanIncomeScrapeR(as.character(files[i, 3]))
   
   mean.income <- mean.income %>%
     mutate(option = as.character(files[i, 1])) %>%
-    mutate(scale = as.character(files[i, 2])) %>%
-    mutate(base = as.character(files[i, 3]))
+    mutate(scale = as.character(files[i, 2]))
   
   final.income <- bind_rows(final.income, mean.income)
    
 }
-# final.income should be 1,536 * 32 = 49,152 rows
+# final.income should be 1,536 * 38 = 58,368 rows
 
 # Remove "Per Capita " and "Equivalent " so the left_join works
 final.income <- final.income %>%
@@ -170,39 +138,40 @@ final.income <- final.income %>%
 
 # Create a df with the baselines
 options <- final.income %>%
-  filter(option != "0") %>%
-  select(-base)
-# should contain 43,008 rows
-# 64*4*6*14*2
+  filter(option != "Payable Law" & option != "Scheduled Law")
+# should contain 52,224 rows
+# 64 subgroups * 4 measures * 6 years * 17 options * 2 scales
 
 # Create a df with the options
 baselines <- final.income %>%
-  filter(option == "0") %>%
-  spread(key = option, value = value) %>%
-  rename(Option0 = `0`)
+  filter(option == "Payable Law" | option == "Scheduled Law") %>%
+  rename(baseline.value = value, baseline.type = option)
 # should contain 6,144 rows
 # 64*4*6*2*2
 
 # left_join the options with the baselines
 final.income <- left_join(options, baselines, by = c("category", "group", "measure", "year", "scale"))
-# should be 86,016 observations
+# should be 104,448 observations
 
 # Calculate the dollar and percent changes
 final.income <- final.income %>%
-  mutate(dollar.change = value - Option0) %>%
-  mutate(percent.change = (value - Option0) / Option0) %>%
-  select(-Option0)
+  mutate(dollar.change = value - baseline.value) %>%
+  mutate(percent.change = (value - baseline.value) / baseline.value) %>%
+  select(-baseline.value)
 
 # Clean up baselines so it matches final.income
 baselines <- baselines %>%
-  rename(value = Option0) %>%
-  mutate(option = "0") %>%
+  rename(value = baseline.value) %>%
+  rename(option = baseline.type) %>%
   mutate(dollar.change = 0) %>%
-  mutate(percent.change = 0)
+  mutate(percent.change = 0) %>%
+  mutate(baseline.type = option)
 
 # Combine the baselines (with zeroes for changes) and the options
-final.income <- union(final.income, baselines)
-# Should be 92,160 observations
+final.income <- union(final.income, baselines) %>%
+  rename(baseline = baseline.type)
+rm(files, mean.income, options, baselines)
+# Should be 110,592 observations
 
 
 # If data directory does not exist, create data directory
